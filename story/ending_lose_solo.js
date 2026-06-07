57493e3a08407fce04bdbc6384c9ed8374c76346
Lucas Lee <ls4lee@uwaterloo.ca>
2026-06-07 18:22:27 -0400

    njhk

---
+++ story/ending_lose_solo.js
@@ -0,0 +1,7 @@
+const endingLoseSolo = {
+  level: "4",
+  type: "lose",
+  title: "Cracked Like A Shell (LOSE)",
+  narrative: "Your boulder disguise fails completely. Squirrels are highly adept at extracting items from hard shells. A gigantic, 300-pound alpha rodent hoists you up and smashes you headfirst against a structural stone wall to crack you open. Earth is permanently dominated by paws.",
+  choices: []
+};
